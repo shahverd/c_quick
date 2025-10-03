5b6344385c8eed135d26097196bdfa6f7f7fa5c0
Mostafa Shahverdy <shahverdy.m@gmail.com>
2025-10-03 08:38:23 +0330

    compile_commands.json

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+#include <glib.h>
 
 int main(){
+
+  GHashTable *ht = g_hash_table_new(g_str_hash, g_str_equal);
+  g_hash_table_insert(ht, "key1", "value1");
 
   int a = 20;
   int b = 22;
